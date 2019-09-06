15ab8540385ae21acdbbc1bf42ac6c52dbba2344
mueezrafiquie <mueezalba@gmail.com>
2019-09-06 14:11:19 -0600

    Update sketch.js

--- firstproject/sketch.js
+++ firstproject/sketch.js
@@ -5,14 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let redAmount = 0;
+let redChangeAmount = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-  background(220);
-  fill(0);
-  ell
-  ellipse(mouseX, mouseY, 100, 100)
+  fill(redAmount, 0, 255);
+  noStroke();
+  ellipse(mouseX, mouseY, 100, 100);
+
+  redAmount += redChangeAmount;
+
+  if (redAmount >= 255) {
+    redChangeAmount *= -1
+  }
+  if (redAmount <= 0) {
+    redChangeAmount *= -1
+  }
+
+  console.log(redAmount);
 }

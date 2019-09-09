65735cbafc31c290e91cc3569f1b010f416183e8
mueezrafiquie <mueezalba@gmail.com>
2019-09-09 14:11:20 -0600

    line art finsihed

--- lineart/sketch.js
+++ lineart/sketch.js
@@ -1,31 +1,17 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Line Art Demo
+// Mueez
+// Sept 9, 2019
 
-let redAmount = 0;
-let redChangeAmount = 1;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background(255);
 }
 
 function draw() {
-  fill(redAmount, 0, 255);
-  noStroke();
-  ellipse(mouseX, mouseY, 100, 100);
+  line(mouseX, mouseY, pmouseX, pmouseY)
 
-  redAmount += redChangeAmount;
 
-  if (redAmount >= 255) {
-    redChangeAmount *= -1
-  }
-  if (redAmount <= 0) {
-    redChangeAmount *= -1
-  }
-
-  console.log(redAmount);
 }

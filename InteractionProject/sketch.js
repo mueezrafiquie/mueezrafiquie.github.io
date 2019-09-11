80ed74510cd90573461cbad4c8aca457159b4eee
mueezrafiquie <mueezalba@gmail.com>
2019-09-11 14:11:25 -0600

    started project

--- InteractionProject/sketch.js
+++ InteractionProject/sketch.js
@@ -7,11 +7,47 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
 }
 
 function draw() {
-  line(mouseX, mouseY, pmouseX, pmouseY)
-
-
+  ellipse(width / 2, height / 1.25, 50, 50)
 }
+
+
+
+
+
+
+
+// let n = 100
+//
+// function mousePressed() {
+//  if (keyIsPressed) {
+//    if (key === "r") {
+//      rect(mouseX, mouseY, n, n)
+//    }
+//    else if (key === "e") {
+//      ellipse(mouseX,mouseY,n,n);
+//    }
+//  }
+// }
+
+// function keyPressed() {
+//  if (key === "b") {
+//    background(0);
+//    stroke(255);
+//    fill(255);
+//  }
+//   else if (key === "w") {
+//     background(255);
+//     stroke(0);
+//     fill(0);
+//   }
+//   else if (keyCode === UP_ARROW) {
+//     n += 5;
+   
+//   }
+//   else if (keyCode === DOWN_ARROW) {
+//     n -= 5;
+//   }
+// }

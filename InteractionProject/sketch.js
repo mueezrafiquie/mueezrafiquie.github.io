96b1975e50c82869f9f3a057d7fafdbb4696d3e8
mueezrafiquie <mueezalba@gmail.com>
2019-09-11 13:48:55 -0600

    Interaction Project Start

---
+++ InteractionProject/sketch.js
@@ -0,0 +1,17 @@
+// Line Art Demo
+// Mueez
+// Sept 9, 2019
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  line(mouseX, mouseY, pmouseX, pmouseY)
+
+
+}

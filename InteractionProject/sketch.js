557679729e67004416503ea2f20ade24dcf145e8
mueezrafiquie <mueezalba@gmail.com>
2019-09-17 14:06:00 -0600

    Added function to keep image in canvas all the time but doesn't adjust with size

--- InteractionProject/sketch.js
+++ InteractionProject/sketch.js
@@ -3,7 +3,6 @@
 // Sept 14, 2019
 
 
-
 let plane;
 let scalar = 1;
 let x;
@@ -18,7 +17,8 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   x = width / 2; 
-  y = height / 1.25;
+  y = height / 1.75;
+  imageMode(CENTER);  //centering image 
 }
 //creating canvas and defining variables in setup()
 
@@ -26,9 +26,12 @@ function setup() {
 function draw() {
   background(255);
 
-  if (keyIsPressed && insideCanvas() === true) {
+  if (keyIsPressed && isInsideCanvas() === true) {
     move();
     //controlling the image with WASD keys 
+  }
+  else {
+    keepInsideCanvas();
   }
 
   image(plane, x, y, plane.width * scalar, plane.height * scalar );
@@ -49,17 +52,41 @@ function mouseWheel() {
 //adding mouseWheel function which will use the same scalar variable to control the size of the image
 
 
-function insideCanvas() {
-  if (x > width || x < 0) {
-    return false
+
+function keepInsideCanvas() {
+  if (keyIsPressed && isInsideCanvas() === "over west"){
+    x += 10;
   }
-  
-  else if (y > height || y < 0) {
-    return false
+  else if (keyIsPressed && isInsideCanvas() === "over north") {
+    y += 10;
+  }
+  else if (keyIsPressed && isInsideCanvas() === "over east") {
+    x -= 10;
+  }
+  else if (keyIsPressed && isInsideCanvas() === "over south") {
+    y -= 10;
+  }
+  // changes the x or y cord to keep image inside of the canvas
+}
+
+
+function isInsideCanvas() {
+  if (x + 150 > width) {
+    return "over east";
+  }
+  else if (x - 150 < 0) {
+    return "over west"
+  }
+  else if (y + 110 > height) {
+    return "over south";
+  }
+  else if (y - 120 < 0) {
+    return "over north";
   }
   else {
-    return true
+    return true;
   }
+  //checks if is image is inside canvas and if not returns which direction it is over 
 }
 
 function move() {
@@ -71,20 +98,21 @@ function move() {
   }
   //using a multiplying factor to change size of image
 
-  if (keyIsDown(87)) {
+  if (keyIsDown(87)) {       //w
     background(255);
     y -= 10;
   }
-  else if (keyIsDown(65)) {
+  else if (keyIsDown(65)) {  //a
     background(255);
     x -= 10;
   }
-  else if (keyIsDown(83)) {
+  else if (keyIsDown(83)) {  //s
     background(255);
     y += 10;
   }
-  else if (keyIsDown(68)) {
+  else if (keyIsDown(68)) {  //d
     background(255);
     x += 10;
   }
+  //chaning x and y cords with WASD keys to move image
 }
